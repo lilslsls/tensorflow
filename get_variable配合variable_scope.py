9378abae92b73e7ef852f1d270f1da89530bc828
lilslsls <1057102550@qq.com>
2019-07-27 09:08:06 +0800

    enter

--- get_variable配合variable_scope.py
+++ get_variable配合variable_scope.py
@@ -8,8 +8,20 @@
 # print("var1:",var1.name)
 # print("var2:",var2.name)
 
-import tensorflow as tf
 
+#嵌套variable_scope
+
+# import tensorflow as tf
+# with tf.variable_scope("test1",):
+#     var1=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
+
+#     with tf.variable_scope("test2",):
+#         var2=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
+# print("var1:",var1.name)
+# print("var2:",var2.name)
+
+#共享变量
+import tensorflow as tf
 with tf.variable_scope("test1",):
     var1=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
 
@@ -17,3 +29,10 @@ with tf.variable_scope("test1",):
         var2=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
 print("var1:",var1.name)
 print("var2:",var2.name)
+
+with tf.variable_scope('test1',reuse=True):
+    var3=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
+    with tf.variable_scope("test2"):
+        var4=tf.get_variable("firstvar",shape=[2],dtype=tf.float32)
+print("var3:",var3.name)
+print("var4:",var1.name)
